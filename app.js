c5fdd7f43d33e2dd34e1cfb756381f38261766f7
SEKTION <64591832+SEKTION@users.noreply.github.com>
2021-05-09 23:53:19 +0530

    JS Logic

--- app.js
+++ app.js
@@ -42,34 +42,3 @@ var text = document.querySelector("textarea");
     pgraph.innerHTML =`Words:${count} Characters:${text.value.length} Lines:${lines}`;
   })
 );
-
-// function countWord() {
-//   var text = document.querySelector('textarea');
-//   var pgraph = document.querySelector("#word-count");
-//   var words = text.value;
-//   var lines = words.split("\n");
-//   lines = lines.length - 1;
-//   for (let i = 0; i < chars.length; i++) {
-//     words = words.replaceAll(chars[i], " ");
-//   }
-//   words = words.split(" ");
-//   var count = 0;
-//   for (var i = 0; i < words.length; i++) {
-//     if (
-//       words[i] != "" &&
-//       words[i] != ";" &&
-//       words[i] != "." &&
-//       words[i] != "," &&
-//       words[i] != ";"
-//     ) {
-//       count += 1;
-//     }
-//   }
-//   pgraph.innerHTML =
-//     "Words:" +
-//     count +
-//     " Characters:" +
-//     text.value.length +
-//     " Lines:" +
-//     lines;
-// }
